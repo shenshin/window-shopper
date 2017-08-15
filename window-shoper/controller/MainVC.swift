be006502f3ffbe2e190d4cbc12b5a10c552703e7
Aleksandr Shenshin <shenshin@me.com>
2017-08-16 00:01:02 +0300

    Initial commit

---
+++ window-shoper/controller/MainVC.swift
@@ -0,0 +1,20 @@
+//
+//  MainVC.swift
+//  window-shoper
+//
+//  Created by Aleksandr Shenshin on 15.08.17.
+//  Copyright © 2017 Aleksandr Shenshin. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
+
